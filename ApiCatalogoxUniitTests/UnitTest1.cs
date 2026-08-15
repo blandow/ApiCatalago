f089655383db0174d727c43b9c93cca90a24281f
blandow <felipeblandowsiqueira@gmail.com>
2026-08-15 20:07:04 -0300

    FEAT Add and configure Unit Tests

---
+++ ApiCatalogoxUniitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiCatalogoxUniitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ApiCatalogoxUnitTests/UnitTests/ProdutosUnitTestsController.cs
@@ -0,0 +1,37 @@
+﻿
+using ApiCatalago.Context;
+using ApiCatalago.DTO.Mappings;
+using ApiCatalago.Repositories;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace ApiCatalogoxUnitTests.UnitTests
+{
+    internal class ProdutosUnitTestsController
+    {
+        public IUnitOfWork repository;
+        public IMapper mapper;
+        public static DbContextOptions<ApiCatalagoContext> dbContextOptions { get; }
+
+        public static string connectionString = "Server=localhost;Database=CatalogoDB;Uid=root;Pwd=Abc/123";
+
+        static ProdutosUnitTestsController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<ApiCatalagoContext>()
+                .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)).Options;
+        }
+        public ProdutosUnitTestsController()
+        {
+            var config = new MapperConfiguration(c => 
+            { 
+                c.AddProfile(new ProdutoDTOMappingProfile()); 
+            }, NullLoggerFactory.Instance);
+
+            mapper = config.CreateMapper();
+
+            var context = new ApiCatalagoContext(dbContextOptions);
+            repository = new UnitOfWork(context);
+        }
+    }
+}
